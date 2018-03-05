b98c9ce69922014eca65be007fefb81c0a0abf62
ashevin <avi.shevin@kik.com>
2018-03-05 16:27:37 +0200

    Add macos target.

--- KinSDK/KinSDK/KinSDK.h
+++ KinSDK/KinSDK/KinSDK.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Kin Foundation. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for KinSDK.
 FOUNDATION_EXPORT double KinSDKVersionNumber;
