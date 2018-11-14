a4ec2e40cac2187804098f8441e8eb4b32ffb78d
Corey Werner <cnotethegr8@gmail.com>
2018-11-14 15:10:46 +0200

    included sodium into project

---
+++ KinSDK/KinSDK/source/third-party/swift-sodium/Examples/OSX/AppDelegate.swift
@@ -0,0 +1,33 @@
+import Cocoa
+import Sodium
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    @IBOutlet weak var window: NSWindow!
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        let sodium = Sodium()
+        let aliceKeyPair = sodium.box.keyPair()!
+        let bobKeyPair = sodium.box.keyPair()!
+        let message = "My Test Message".bytes
+
+        print("Original Message:\(message.utf8String!)")
+
+        let encryptedMessageFromAliceToBob: Bytes =
+            sodium.box.seal(
+                message: message,
+                recipientPublicKey: bobKeyPair.publicKey,
+                senderSecretKey: aliceKeyPair.secretKey)!
+
+        print("Encrypted Message:\(encryptedMessageFromAliceToBob)")
+
+        let messageVerifiedAndDecryptedByBob =
+            sodium.box.open(
+                nonceAndAuthenticatedCipherText: encryptedMessageFromAliceToBob,
+                senderPublicKey: bobKeyPair.publicKey,
+                recipientSecretKey: aliceKeyPair.secretKey)
+
+        print("Decrypted Message:\(messageVerifiedAndDecryptedByBob!.utf8String!)")
+
+    }
+}
